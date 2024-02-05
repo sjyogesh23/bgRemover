27a8b8182c0513b4fff6d7bd8b8826926ed74954
SJ Yogesh <102499551+sjyogesh23@users.noreply.github.com>
2024-02-05 18:46:45 +0530

    removing panorama

--- bgremover/bgremo.py
+++ bgremover/bgremo.py
@@ -14,7 +14,11 @@ imglist = []
 for imgpath in allimgs:
     img = cv2.imread(f'Images/{imgpath}')
     imglist.append(img)
-    
+print("""
+        1. Press A - Previous image
+        2. Press D - Next Image
+        3. Press Q - Quit
+    """) 
 imgindex = 0
 
 while True:
@@ -22,24 +26,28 @@ while True:
         
     if len(imglist) > 0:
         imgbg = cv2.resize(imglist[imgindex], (img.shape[1], img.shape[0]))
-        imgout = seg.removeBG(img, imgbg, cutThreshold=0.7)
+        imgout = seg.removeBG(img, imgbg, cutThreshold=0.5)
     else:
         imgout = seg.removeBG(img, (255,255,255), cutThreshold=0.7)
     
     
     opscreen = cvzone.stackImages([img, imgout],2,1)
     cv2.imshow("Image", opscreen)
+    
     key = cv2.waitKey(1)
+    
     if key == ord('a'):
         if imgindex>0:
             imgindex -= 1
         else:
             imgindex = len(imglist) - 1
-    elif key == ord('d'):
+    if key == ord('d'):
         if imgindex<len(imglist) - 1:
             imgindex += 1
         else:
             imgindex = 0
     elif key == ord('q'):
         break
-    print(imgindex)
+        
+cap.release()
+cv2.destroyAllWindows()
